fd7619e43431fc517c60a9fa71fa5b75348416e9
Guido Zockoll <gzockoll@gmail.com>
2012-03-21 06:09:10 +0100

    example

--- src/main/java/de/gzockoll/camel/proxy/MyRouteBuilder.java
+++ src/main/java/de/gzockoll/camel/proxy/MyRouteBuilder.java
@@ -4,10 +4,12 @@ import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
 
 public class MyRouteBuilder extends RouteBuilder {
+	private static final String TRANSPORT = "seda";
 
 	@Override
 	public void configure() throws Exception {
-		from("jetty:http://localhost:9999/piwik")
+		from(
+				"jetty:http://localhost:9999?bridgeEndpoint=true&matchOnUriPrefix=true")
 				.onException(Exception.class)
 				.handled(true)
 				// create a custom failure response
@@ -18,11 +20,11 @@ public class MyRouteBuilder extends RouteBuilder {
 				.end()
 				// now just force an exception immediately
 				.to("log:com.mycompany.order?level=DEBUG&groupInterval=10000&groupDelay=60000&groupActiveOnly=false")
-				.throttle(constant(3)).to("vm:forward");
+				.throttle(constant(3)).to(TRANSPORT + ":forward");
 
-		from("vm:forward?concurrentConsumers=3").to(
-				"log:com.mycompany.order?showAll=true&multiline=true").to(
-				"http://www.bvsh.org?bridgeEndpoint=true");
+		from(TRANSPORT + ":forward?concurrentConsumers=40")
+				.to("log:com.mycompany.order?showAll=true&multiline=true")
+				.to("http://www.bvsh.org/?bridgeEndpoint=true&throwExceptionOnFailure=false");
 
 	}
 }
